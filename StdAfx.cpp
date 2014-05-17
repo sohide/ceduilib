a19092841c176699f6c3c47d93a3a7ed89e90fd4
soyzhc <soyzhc@soyzhcdeMacBook-Pro.local>
2014-05-17 10:01:10 +0800

    create new

---
+++ StdAfx.cpp
@@ -0,0 +1,13 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	UIlib.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "StdAfx.h"
+
+
+#if 0
+
+#pragma comment( lib, "winmm.lib" )
+#pragma comment( lib, "comctl32.lib" )
+
+#endif
